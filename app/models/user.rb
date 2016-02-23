088768c2bfdea107a38c3ebe4de7d13e84c4c3df
Meredith Heller <meredith.a.heller@gmail.com>
2016-02-23 09:31:35 -0800

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ActiveRecord::Base
+	validates :username, :password_digest, :session_token, presence: true
+	validates :password, length: { minimum: 6, allow_nil: true }
+
+	attr_reader :password
+
+	after_initialize :ensure_session_token
+
+	def self.find_by_credentials(username, password)
+		user = User.find_by(username: username)
+		return user if user && user.valid_password?(password)
+		nil
+	end
+
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def valid_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
+	def reset_token!
+		self.session_token = SecureRandom.urlsafe_based64
+		self.save!
+		self.session_token
+	end
+
+	private
+	def ensure_session_token
+		self.session_token ||= SecureRandom.urlsafe_based64
+	end
+
+
+
+end
